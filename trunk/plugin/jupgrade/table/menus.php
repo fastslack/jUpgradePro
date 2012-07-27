a00f66402022bd5be87cc6395bfb8459b38f13f4
Matias Aguirre <matias@redcomponent.com>
2012-07-27 07:53:03 +0000

    Small fixes on menus

--- trunk/plugin/jupgrade/table/menus.php
+++ trunk/plugin/jupgrade/table/menus.php
@@ -31,6 +31,8 @@ class JUpgradeTableMenus extends JUpgradeTable
 	/** @var string */
 	var $name				= null;
 	/** @var string */
+	var $title				= null;
+	/** @var string */
 	var $alias				= null;
 	/** @var string */
 	var $link				= null;
@@ -43,29 +45,23 @@ class JUpgradeTableMenus extends JUpgradeTable
 	/** @var int */
 	var $parent				= null;
 	/** @var int */
-	var $sublevel			= null;
+	var $parent_id				= null;
 	/** @var int */
 	var $ordering			= null;
 	/** @var boolean */
 	var $checked_out		= 0;
 	/** @var datetime */
 	var $checked_out_time	= 0;
-	/** @var boolean */
-	var $pollid				= null;
 	/** @var string */
 	var $browserNav			= null;
 	/** @var int */
 	var $access				= null;
-	/** @var int */
-	var $utaccess			= null;
 	/** @var string */
 	var $params				= null;
-	/** @var int Pre-order tree traversal - left value */
-	var $lft				= null;
-	/** @var int Pre-order tree traversal - right value */
-	var $rgt				= null;
 	/** @var int */
 	var $home				= null;
+	/** @var int */
+	var $option				= null;
 
 	/**
 	 * Table type
@@ -96,6 +92,9 @@ class JUpgradeTableMenus extends JUpgradeTable
 		$conditions = array();
 		
 		$conditions['as'] = "m";
+		
+		$conditions['select'] = 'm.id, m.menutype, m.name, m.name AS title, m.alias, m.link, m.type, c.option, m.published, m.parent AS parent_id,'
+			.' m.sublevel AS level, m.ordering, m.checked_out, m.checked_out_time, m.browserNav, m.access, m.params, m.home';
 		
 		$join = array();
 		$join[] = "LEFT JOIN #__components AS c ON c.id = m.componentid";
@@ -148,15 +147,15 @@ class JUpgradeTableMenus extends JUpgradeTable
         if (preg_match('|id=([0-9]+)|', $this->link, $tmp)) {
 
           $id = $tmp[1];
-          /*
-          if ( (strpos($this->link, 'layout=blog') !== false) AND
-             ( (strpos($this->link, 'view=category') !== false) OR
-               (strpos($this->link, 'view=section') !== false) ) ) {
-          				$this->link = 'index.php?option=com_content&view=category&layout=blog&id='.$$id;
-          } elseif (strpos($this->link, 'view=section') !== false) {
-            $this->link = 'index.php?option=com_content&view=category&layout=blog&id='.$id;
-          }
-          */
+          
+          //if ( (strpos($this->link, 'layout=blog') !== false) AND
+          //   ( (strpos($this->link, 'view=category') !== false) OR
+          //     (strpos($this->link, 'view=section') !== false) ) ) {
+          //				$this->link = 'index.php?option=com_content&view=category&layout=blog&id='.$$id;
+          //} elseif (strpos($this->link, 'view=section') !== false) {
+          //  $this->link = 'index.php?option=com_content&view=category&layout=blog&id='.$id;
+          //}
+          
         }
       }
     }

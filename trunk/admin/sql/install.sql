6cb014bfbadccd5fd7dacde59d999d5317943db5
Matias Aguirre <matias@redcomponent.com>
2012-08-14 09:55:45 +0000

    Updating the installation sql

--- trunk/admin/sql/install.sql
+++ trunk/admin/sql/install.sql
@@ -33,26 +33,40 @@ DROP TABLE IF EXISTS `jupgrade_steps`;
 CREATE TABLE IF NOT EXISTS `jupgrade_steps` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(255) NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `cid` int(11) NOT NULL DEFAULT '0',
+  `class` varchar(255) NOT NULL,
+  `category` varchar(255) NOT NULL DEFAULT '0',
   `status` int(11) NOT NULL,
   `extension` int(1) NOT NULL DEFAULT '0',
   `state` text NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=11 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 ;
 
 --
 -- Dumping data for table `jupgrade_steps`
 --
 
-INSERT INTO `jupgrade_steps` VALUES(1, 'users', 0, 0, '');
-INSERT INTO `jupgrade_steps` VALUES(2, 'categories', 0, 0, '');
-INSERT INTO `jupgrade_steps` VALUES(3, 'content', 0, 0, '');
-INSERT INTO `jupgrade_steps` VALUES(4, 'menus', 0, 0, '');
-INSERT INTO `jupgrade_steps` VALUES(5, 'modules', 0, 0, '');
-INSERT INTO `jupgrade_steps` VALUES(6, 'banners', 0, 0, '');
-INSERT INTO `jupgrade_steps` VALUES(7, 'contacts', 0, 0, '');
-INSERT INTO `jupgrade_steps` VALUES(8, 'newsfeeds', 0, 0, '');
-INSERT INTO `jupgrade_steps` VALUES(9, 'weblinks', 0, 0, '');
-INSERT INTO `jupgrade_steps` VALUES(10, 'extensions', 0, 1, '');
+INSERT INTO `jupgrade_steps` (`id`, `name`, `title`, `cid`, `class`, `category`, `status`, `extension`, `state`) VALUES
+(1, 'users', 'Users', 0, 'jUpgradeUsers', 0, 0, 0, ''),
+(2, 'arogroup', 'Users Groups', 0, 'jUpgradeUsergroups', 0, 0, 0, ''),
+(3, 'usergroupmap', 'Users Groups', 0, 'jUpgradeUsergroupMap', 0, 0, 0, ''),
+(4, 'categories', 'Categories', 0, 'jUpgradeCategories', 0, 0, 0, ''),
+(5, 'sections', 'Sections', 0, 'jUpgradeSections', 0, 0, 0, ''),
+(6, 'contents', 'Contents', 0, 'jUpgradeContent', 0, 0, 0, ''),
+(7, 'contents_frontpage', 'FrontPage Contents', 0, 'jUpgradeContentFrontpage', 0, 0, 0, ''),
+(8, 'menus', 'Menus', 0, 'jUpgradeMenu', 0, 0, 0, ''),
+(9, 'menus_types', 'Menus Types', 0, 'jUpgradeMenusTypes', 0, 0, 0, ''),
+(10, 'modules', 'Core Modules', 0, 'jUpgradeModules', 0, 0, 0, ''),
+(11, 'modules_menu', 'Modules Menus', 0, 'jUpgradeModulesMenu', 0, 0, 0, ''),
+(12, 'banners', 'Banners', 0, 'jUpgradeBanners', 'com_banner', 0, 0, ''),
+(13, 'banners_clients', 'Banners Clients', 0, 'jUpgradeBannersClients', 0, 0, 0, ''),
+(14, 'banners_tracks', 'Banners Tracks', 0, 'jUpgradeBannersTracks', 0, 0, 0, ''),
+(15, 'contacts', 'Contacts', 0, 'jUpgradeContacts', 'com_contact_details', 0, 0, ''),
+(16, 'newsfeeds', 'NewsFeeds', 0, 'jUpgradeNewsfeeds', 'com_newsfeeds', 0, 0, ''),
+(17, 'weblinks', 'Weblinks', 0, 'jUpgradeWeblinks', 'com_weblinks', 0, 0, ''),
+(18, 'extensions', 'Extensions', 0, '', 0, 0, 1, '');
+
 
 --
 -- Table structure for table `jupgrade_modules`

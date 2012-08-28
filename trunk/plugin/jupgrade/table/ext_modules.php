b35b2876c6d1a2e4a2ba52a10000959128d96cdc
Matias Aguirre <matias@redcomponent.com>
2012-08-28 08:09:05 +0000

    Adding new tables for plugin

---
+++ trunk/plugin/jupgrade/table/ext_modules.php
@@ -0,0 +1,103 @@
+<?php
+/**
+* @version $Id:
+* @package Matware.jUpgradePro
+* @copyright Copyright (C) 2005 - 2012 Matware. All rights reserved.
+* @author Matias Aguirre
+* @email maguirre@matware.com.ar
+* @link http://www.matware.com.ar/
+* @license GNU General Public License version 2 or later; see LICENSE
+*/
+
+// Check to ensure this file is within the rest of the framework
+defined('JPATH_BASE') or die();
+
+/**
+ * Menu table
+ *
+ * @package 	Joomla.Framework
+ * @subpackage		Table
+ * @since	1.0
+ */
+class JUpgradeTableExt_modules extends JUpgradeTable
+{
+	/** @var int Primary key */
+	var $id					= null;
+	/** @var string */
+	var $title				= null;
+	/** @var string */
+	var $showtitle			= null;
+	/** @var int */
+	var $content			= null;
+	/** @var int */
+	var $ordering			= null;
+	/** @var string */
+	var $position			= null;
+	/** @var boolean */
+	var $checked_out		= 0;
+	/** @var time */
+	var $checked_out_time	= 0;
+	/** @var boolean */
+	var $published			= null;
+	/** @var string */
+	var $module				= null;
+	/** @var int */
+	var $access				= null;
+	/** @var string */
+	var $params				= null;
+	/** @var string */
+	var $client_id			= null;
+	/** @var int */
+	var $element				= null;
+	/** @var int */
+	var $type				= null;
+
+	/**
+	 * Table type
+	 *
+	 * @var string
+	 */	
+	var $_type = 'ext_modules';	
+
+	/**
+	 * Contructor
+	 *
+	 * @access protected
+	 * @param database A database connector object
+	 */
+	function __construct( &$db ) {
+		parent::__construct( '#__modules', 'id', $db );
+	}
+
+	/**
+	 * Setting the conditions hook
+	 *
+	 * @return	void
+	 * @since	3.0.0
+	 * @throws	Exception
+	 */
+	public function getConditionsHook()
+	{
+		$conditions = array();
+
+		$conditions['select'] = '`title` AS name, `module` AS element, params';
+
+		$conditions['where'][] = "module NOT IN ('mod_mainmenu',   'mod_login',   'mod_popular',   'mod_latest',   'mod_stats',   'mod_unread',   'mod_online',   'mod_toolbar',   'mod_quickicon',   'mod_logged',   'mod_footer',   'mod_menu',   'mod_submenu',   'mod_status',   'mod_title',   'mod_login' )";
+				
+		return $conditions;
+	}
+
+	/**
+	 * 
+	 *
+	 * @access	public
+	 * @param		Array	Result to migrate
+	 * @return	Array	Migrated result
+	 */
+	function migrate( )
+	{		
+		$this->params = isset($this->params) ? $this->convertParams($this->params) : '';
+		// Default
+		$this->type = 'module';
+	}
+}

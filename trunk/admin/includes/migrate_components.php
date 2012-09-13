d5a1a850b594a0af1cf7331f68c3f8d0ca2dc56f
Matias Aguirre <maguirre@matware.com.ar>
2012-09-13 00:56:37 -0300

    Adding files for extensions

---
+++ trunk/admin/includes/migrate_components.php
@@ -0,0 +1,91 @@
+<?php
+/**
+ * jUpgrade
+ *
+ * @version		  $Id$
+ * @package		  MatWare
+ * @subpackage	com_jupgrade
+ * @author      Matias Aguirre <maguirre@matware.com.ar>
+ * @link        http://www.matware.com.ar
+ * @copyright		Copyright 2006 - 2012 Matias Aguire. All rights reserved.
+ * @license		  GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+/**
+ * Upgrade class for Weblinks
+ *
+ * This class takes the weblinks from the existing site and inserts them into the new site.
+ *
+ * @since	3.0.0
+ */
+class jUpgradeComponents extends jUpgrade
+{
+	/**
+	 * @var		string	The name of the source database table.
+	 * @since	3.0.0
+	 */
+	protected $source = '#__components';
+
+	/**
+	 * @var		string	The name of the destination database table.
+	 * @since	3.0.0
+	 */
+	protected $destination = '#__extensions';
+
+	/**
+	 * @var		string	The name of the source database table.
+	 * @since	3.0.0
+	 */
+	protected $_tbl_key = 'id';
+
+	/**
+	 * Setting the conditions hook
+	 *
+	 * @return	void
+	 * @since	3.0.0
+	 * @throws	Exception
+	 */
+	public function getConditionsHook()
+	{
+		$conditions = array();
+		
+		$conditions['as'] = "c";
+		
+		$conditions['select'] = 'name, `option` AS element, params';
+
+		$where = array();
+		$where[] = "c.parent = 0";
+		$where[] = "c.option NOT IN ('com_admin', 'com_banners', 'com_cache', 'com_categories', 'com_checkin', 'com_config', 'com_contact', 'com_content', 'com_cpanel', 'com_frontpage', 'com_installer', 'com_jupgrade', 'com_languages', 'com_login', 'com_mailto', 'com_massmail', 'com_media', 'com_menus', 'com_messages', 'com_modules', 'com_newsfeeds', 'com_plugins', 'com_poll', 'com_search', 'com_sections', 'com_templates', 'com_user', 'com_users', 'com_weblinks', 'com_wrapper' )";
+		
+		$conditions['where'] = $where;
+		
+		return $conditions;
+	}
+
+	/**
+	 * Get the raw data for this part of the upgrade.
+	 *
+	 * @return	array	Returns a reference to the source data array.
+	 * @since	3.0.0
+	 * @throws	Exception
+	 */
+	public function &getSourceDatabase()
+	{
+		// Getting the rows
+		$rows = parent::getSourceDatabase();
+
+		// Do some custom post processing on the list.
+		foreach ($rows as &$row)
+		{
+			$row = (array) $row;
+			// Converting params to JSON
+			$row['params'] = $this->convertParams($row['params']);
+			// Defaults
+			$row['type'] = 'component';
+			$row['client_id'] = 1;
+		}
+
+		return $rows;
+	}
+	
+}
---
+++ trunk/admin/includes/migrate_plugins.php
@@ -0,0 +1,90 @@
+<?php
+/**
+ * jUpgrade
+ *
+ * @version		  $Id$
+ * @package		  MatWare
+ * @subpackage	com_jupgrade
+ * @author      Matias Aguirre <maguirre@matware.com.ar>
+ * @link        http://www.matware.com.ar
+ * @copyright		Copyright 2006 - 2012 Matias Aguire. All rights reserved.
+ * @license		  GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+/**
+ * Upgrade class for Weblinks
+ *
+ * This class takes the weblinks from the existing site and inserts them into the new site.
+ *
+ * @since	3.0.0
+ */
+class jUpgradePlugins extends jUpgrade
+{
+	/**
+	 * @var		string	The name of the source database table.
+	 * @since	3.0.0
+	 */
+	protected $source = '#__plugins';
+
+	/**
+	 * @var		string	The name of the destination database table.
+	 * @since	3.0.0
+	 */
+	protected $destination = '#__extensions';
+
+	/**
+	 * @var		string	The name of the source database table.
+	 * @since	3.0.0
+	 */
+	protected $_tbl_key = 'id';
+
+	/**
+	 * Setting the conditions hook
+	 *
+	 * @return	void
+	 * @since	3.0.0
+	 * @throws	Exception
+	 */
+	public function getConditionsHook()
+	{
+		$conditions = array();
+		
+		$conditions['as'] = "p";
+		
+		$conditions['select'] = 'name, \'plugin\' AS type, element, folder, client_id, ordering, params';
+
+		$where = array();
+		$where[] = "element   NOT   IN   ('joomla',   'ldap',   'gmail',   'openid',   'content',   'categories',   'contacts',   'sections',   'newsfeeds',   'weblinks',   'pagebreak',   'vote',   'emailcloak',   'geshi',   'loadmodule',   'pagenavigation', 'none',   'tinymce',   'xstandard',   'image',   'readmore',   'sef',   'debug',   'legacy',   'cache',   'remember', 'backlink', 'log', 'blogger', 'mtupdate' )";
+		
+		$conditions['where'] = $where;
+
+		$conditions['group_by'] = 'element';
+		
+		return $conditions;
+	}
+
+	/**
+	 * Get the raw data for this part of the upgrade.
+	 *
+	 * @return	array	Returns a reference to the source data array.
+	 * @since	3.0.0
+	 * @throws	Exception
+	 */
+	public function &getSourceDatabase()
+	{
+		// Getting the rows
+		$rows = parent::getSourceDatabase();
+
+		// Do some custom post processing on the list.
+		foreach ($rows as &$row)
+		{
+			$row = (array) $row;
+			// Converting params to JSON
+			$row['params'] = $this->convertParams($row['params']);
+			// Defaults
+			$row['type'] = 'plugin';
+		}
+
+		return $rows;
+	}
+}

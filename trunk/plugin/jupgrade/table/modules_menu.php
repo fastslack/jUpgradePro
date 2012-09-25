a9e143e42a730c5e4801dfcd99bb19a799c4fb85
Matias Aguirre <maguirre@matware.com.ar>
2012-09-25 01:22:44 -0300

    Adding modules menu

---
+++ trunk/plugin/jupgrade/table/modules_menu.php
@@ -0,0 +1,78 @@
+<?php
+/**
+* @version		$Id: module.php 14401 2010-01-26 14:10:00Z louis $
+* @package		Joomla.Framework
+* @subpackage	Table
+* @copyright	Copyright (C) 2005 - 2010 Open Source Matters. All rights reserved.
+* @license		GNU/GPL, see LICENSE.php
+* Joomla! is free software. This version may have been modified pursuant
+* to the GNU General Public License, and as distributed it includes or
+* is derivative of works licensed under the GNU General Public License or
+* other free or open source software licenses.
+* See COPYRIGHT.php for copyright notices and details.
+*/
+
+// Check to ensure this file is within the rest of the framework
+defined('JPATH_BASE') or die();
+
+/**
+ * Module table
+ *
+ * @package 	Joomla.Framework
+ * @subpackage		Table
+ * @since	1.0
+ */
+class JUpgradeTableModules_menu extends JUpgradeTable
+{
+	/** @var int Primary key */
+	var $moduleid					= null;
+	/** @var string */
+	var $menuid				= null;
+
+	/**
+	 * Table type
+	 *
+	 * @var string
+	 */	
+	var $_type = 'modules_menu';
+
+	/**
+	 * Second key
+	 *
+	 * @var string
+	 */	
+	var $_tmp_key = 'menuid';
+
+	/**
+	 * Contructor
+	 *
+	 * @access protected
+	 * @param database A database connector object
+	 */
+	function __construct( &$db ) {
+		parent::__construct( '#__modules_menu', 'moduleid', $db );
+	}
+
+	/**
+	 * Setting the conditions hook
+	 *
+	 * @return	void
+	 * @since	3.0.0
+	 * @throws	Exception
+	 */
+	public function getConditionsHook()
+	{
+		$conditions = array();
+
+		$conditions['as'] = "m";
+
+		$conditions['select'] = "m.moduleid, m.menuid";
+
+		$conditions['join'][] = "LEFT JOIN #__modules AS modules ON modules.id = m.moduleid";
+
+		$conditions['where'][] = "m.moduleid NOT IN (1,2,3,4,8,13,14,15)";
+		$conditions['where'][] = "modules.module IN ('mod_breadcrumbs', 'mod_footer', 'mod_mainmenu', 'mod_menu', 'mod_related_items', 'mod_stats', 'mod_wrapper', 'mod_archive', 'mod_custom', 'mod_latestnews', 'mod_mostread', 'mod_search', 'mod_syndicate', 'mod_banners', 'mod_feed', 'mod_login', 'mod_newsflash', 'mod_random_image', 'mod_whosonline' )";
+				
+		return $conditions;
+	}
+}

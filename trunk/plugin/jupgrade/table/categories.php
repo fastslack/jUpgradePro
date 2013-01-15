55d045dac0890e57d709f22d8d95040377172170
Matias Aguirre <maguirre@matware.com.ar>
2013-01-15 03:15:42 -0300

    Fixing categories order on rest method

--- trunk/plugin/jupgrade/table/categories.php
+++ trunk/plugin/jupgrade/table/categories.php
@@ -87,7 +87,7 @@ class JUpgradeTableCategories extends JUpgradeTable
 		$where_or[] = "section REGEXP '^[\\-\\+]?[[:digit:]]*\\.?[[:digit:]]*$'";
 		$where_or[] = "section IN ('com_banner', 'com_contact', 'com_contact_details', 'com_content', 'com_newsfeeds', 'com_sections', 'com_weblinks' )";
 
-		$conditions['order'] = "section DESC, ordering DESC";		
+		$conditions['order'] = "id DESC, section DESC, ordering DESC";		
 		$conditions['where_or'] = $where_or;
 		
 		return $conditions;

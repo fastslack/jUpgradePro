bf6bce74a0bcbd516ab3da6c8a70a8f326688593
Matias Aguirre <maguirre@matware.com.ar>
2013-03-05 13:16:13 -0300

    Fixing the generic table to accept multiple keys

--- trunk/plugin/jupgrade/table/generic.php
+++ trunk/plugin/jupgrade/table/generic.php
@@ -13,11 +13,11 @@
 defined('JPATH_BASE') or die();
 
 /**
- * Menu table
+ * Generic table
  *
- * @package 	Joomla.Framework
- * @subpackage		Table
- * @since	1.0
+ * @package 	Matware.jUpgradePro
+ * @subpackage		JUpgradeTableGeneric
+ * @since	3.0.1
  */
 class JUpgradeTableGeneric extends JUpgradeTable
 {
@@ -39,22 +39,40 @@ class JUpgradeTableGeneric extends JUpgradeTable
 	}
 
 	/**
+	 * Setting the conditions hook
+	 *
+	 * @return	void
+	 * @since	3.0.1
+	 * @throws	Exception
+	 */
+	public function getConditionsHook()
+	{
+		$conditions = array();
+		$table = $this->getTableName();
+
+		$keys = $this->_getTableKeys($table);
+
+		$conditions['order'] = count( $keys ) ? implode( ', ', $keys ) : '';
+		
+		return $conditions;
+	}
+
+	/**
 	 * Change the generic table to new one
 	 *
 	 * @return	void
-	 * @since	3.0.0
+	 * @since	3.0.1
 	 * @throws	Exception
 	 */
 	public function changeTable($table)
 	{
 		// Getting table
+		$name = $table;
 		$table = '#__'.$table;
 
 		// Getting key
-		$query = "SHOW KEYS FROM {$table} WHERE Key_name = 'PRIMARY'";
-		$this->_db->setQuery( $query );
-		$keys = $this->_db->loadObjectList();
-		$key = !empty($keys) ? $keys[0]->Column_name : '';
+		$keys = $this->_getTableKeys($table);
+		$key = !empty($keys) ? $keys[0] : '';
 
 		// Getting columns
 		$this->_db->setQuery("SHOW COLUMNS FROM {$table}");
@@ -65,6 +83,39 @@ class JUpgradeTableGeneric extends JUpgradeTable
 			$this->$colname = '';
 		}
 
+		// Check if table exists on db
+		$query = "SELECT name FROM jupgrade_plugin_steps WHERE name = '{$name}'";
+		$this->_db->setQuery( $query );
+		$exists = $this->_db->loadResult();
+
+		if ($exists == '') {
+			$query = "INSERT INTO jupgrade_plugin_steps (`name`) VALUES ( '{$name}' )  ";
+			$this->_db->setQuery( $query );
+			$this->_db->query();
+		}
+
 		parent::__construct( $table, $key, $this->_db );
 	}
+
+	/**
+	 * Get the keys of the generic table
+	 *
+	 * @return	void
+	 * @since	3.0.1
+	 * @throws	Exception
+	 */
+	private function _getTableKeys($table)
+	{
+		$query = "SHOW KEYS FROM {$table} WHERE Key_name = 'PRIMARY'";
+		$this->_db->setQuery( $query );
+		$result = $this->_db->loadObjectList();
+
+		$return = array();
+		foreach ($result as $key) {
+			$return[] = $key->Column_name;
+		}
+
+		return $return;
+	}
+
 }

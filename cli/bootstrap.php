2e9c065d4c6bc8a0640b45a1e3712464d91f76fb
Matias Aguirre <maguirre@matware.com.ar>
2013-05-16 17:57:49 -0300

    Bootstrap fixes

--- cli/bootstrap.php
+++ cli/bootstrap.php
@@ -10,30 +10,37 @@
 * @link http://www.matware.com.ar/
 * @license GNU General Public License version 2 or later; see LICENSE
 */
-/*
- * Change the following path to suit where you have cloned or installed the Joomla Platform repository.
- * The default setting assumes you have the joomla platform and examples folder at the same level.
- */
 
+// User define for Joomla! 2.5 / 3.0 / 3.1 path
+define('JPATH_SITE', dirname(dirname(dirname(__FILE__))).'/joomla-cms');
+
+// @@@ DONT TOUCH BELOW THIS @@@
 // Setup the base path related constant.
 define('JPATH_BASE', dirname(__FILE__));
-define('JPATH_SITE', dirname(__FILE__));
 define('JPATH_ROOT', dirname(__FILE__));
-define('JPATH_PLUGINS', dirname(__FILE__).'/plugins');
-define('JPATH_LIBRARIES', dirname(dirname(dirname(__FILE__))).'/joomla-cms.my/libraries'   );
-define('JPATH_CACHE', dirname(dirname(dirname(__FILE__))).'/joomla-cms.my/cache'   );
+define('JPATH_PLUGINS', JPATH_SITE.'/plugins');
+define('JPATH_ADMINISTRATOR', JPATH_SITE.'/administrator'   );
+define('JPATH_LIBRARIES', JPATH_SITE.'/libraries'   );
+define('JPATH_INSTALLATION', JPATH_SITE.'/installation'   );
+define('JPATH_CACHE', JPATH_SITE.'/cache'   );
 define('JPATH_COMPONENT', dirname(dirname(__FILE__)).'/trunk/admin' );
 define('JPATH_COMPONENT_ADMINISTRATOR', dirname(dirname(__FILE__)).'/trunk/admin');
 
 // Import the Joomla! Platform
-require JPATH_LIBRARIES.'/import.legacy.php';
-// Import the JCli class from the platform.
+if (file_exists(JPATH_LIBRARIES.'/import.legacy.php')) {
+	require_once JPATH_LIBRARIES.'/import.legacy.php';
+} else if (file_exists(JPATH_LIBRARIES.'/import.php')) {
+	require_once JPATH_LIBRARIES.'/import.php';
+}
+
+// Import the Joomla! classes from the platform.
 jimport('joomla.application.cli');
-// Require the files
 jimport('joomla.filesystem.file');
 jimport('joomla.database.database');
+jimport('joomla.environment.request');
+jimport('cms.model.legacy');
 
-// Require the files
+// Require the jUpgradePro files
 require_once JPATH_COMPONENT.'/models/checks.php';
 require_once JPATH_COMPONENT.'/models/cleanup.php';
 require_once JPATH_COMPONENT.'/models/step.php';

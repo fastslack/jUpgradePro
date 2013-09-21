03696d8ffa91c19b81287a5693b38a9fa5f13d5b
klas <klas.berlic@gmail.com>
2013-09-21 23:07:19 +0200

    Change array indexes to match existing ones

--- trunk/admin/includes/schemas/joomla15/menus.php
+++ trunk/admin/includes/schemas/joomla15/menus.php
@@ -153,14 +153,14 @@ class jUpgradeMenu extends jUpgrade
 			$row['access']++;
 
 			// Fixing level
-			$row['level']++;
+			$row['sublevel']++;
 
 			// Fixing language
 			$row['language'] = '*';
 
 			// Fixing parent_id
-			if ($row['parent_id'] == 0) {
-				$row['parent_id'] = 1;
+			if ($row['parent'] == 0) {
+				$row['parent'] = 1;
 			}
 
       // Converting params to JSON

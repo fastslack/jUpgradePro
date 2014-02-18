b320848f553c75d50ba7536d349054c9e8514e0b
Matias Aguirre <maguirre@matware.com.ar>
2014-02-18 15:17:10 -0300

    Fix call static method on database driver

--- trunk/admin/includes/driver/database.php
+++ trunk/admin/includes/driver/database.php
@@ -50,11 +50,7 @@ class JUpgradeproDriverDatabase extends JUpgradeproDriver
 		JUpgradeproHelper::requireClass($name, $xmlpath, $class);
 
 		// @@ Fix bug using PHP < 5.2.3 version
-		if (version_compare(PHP_VERSION, '5.2.3', '<')) {
-			$this->_conditions = call_user_func($class .'::getConditionsHook');
-		}else{
-			$this->_conditions = $class::getConditionsHook();
-		}
+		$this->_conditions = call_user_func(array($class, 'getConditionsHook'));
 
 		$db_config = array();
 		$db_config['driver'] = $this->params->old_dbtype;

991bfce1fda35a9aa3f9315c84836014d273641a
Matias Aguirre <maguirre@matware.com.ar>
2014-02-17 23:27:52 -0300

    Fix menu migration from J! 1.5

--- trunk/admin/includes/schemas/1.5/menus.php
+++ trunk/admin/includes/schemas/1.5/menus.php
@@ -46,92 +46,76 @@ class JUpgradeproMenu extends JUpgradepro
 		return $conditions;
 	}
 
-	/*
-	 * Fake method after hooks
-	 *
-	 * @return	void
-	 * @since	3.0.0
-	 * @throws	Exception
-	 */
-	public function afterHook()
-	{
-		$this->insertDefaultMenus();
-	}
-
 	/**
-	 * Insert the default menus deleted on cleanups to maintain the original id's
+	 * Method to be called before migrate any data
 	 *
-	 * @return	void
-	 * @since	0.5.2
+	 * @return	array
+	 * @since	3.2.0
 	 * @throws	Exception
 	 */
-	public static function insertDefaultMenus()
+	public function beforeHook()
 	{
-		jimport('joomla.table.table');
+		// Insert needed value
+		$query = $this->_db->getQuery(true);
+		$query->insert('#__jupgradepro_menus')->columns('`old`, `new`')->values("0, 0");
 
-		// Getting the database instance
-		$db = JFactory::getDbo();
-		$query = $db->getQuery(true);
-		// Getting the table and query
-		$table = JTable::getInstance('Menu', 'JTable');
-
-		// Getting the data
-		$query->select('`menutype`, `title`, `alias`, `note`, `path`, `link`, `type`, `published`, `parent_id`, `component_id`, `ordering`, `checked_out`, `checked_out_time`, `browserNav`, `access`, `img`, `template_style_id`, `params`, `home`, `language`, `client_id`');
-		$query->from('#__jupgradepro_default_menus');
-		$query->order('id ASC');
-		$db->setQuery($query);
-		$menus = $db->loadAssocList();
-
-		foreach ($menus as $menu) {
-
-			// Unset id
-			$menu['id'] = 0;
-
-			// Getting the data
-			$query->clear();
-			$query->select('alias');
-			$query->from('#__menu');
-			$query->where("alias LIKE '{$menu['alias']}%'");
-			$query->order('id DESC');
-			$query->limit(1);
-			$db->setQuery($query);
-			$alias = $db->loadResult();
-
-			// Prevent MySQL duplicate error
-			// @@ Duplicate entry for key 'idx_client_id_parent_id_alias_language'
-			$menu['alias'] = (!empty($alias)) ? $alias."~" : $menu['alias'];
-
-			// Looking for parent
-			$parent = 1;
-			$explode = explode("/", $menu['path']);
-
-			if (count($explode) > 1) {
-
-				$query->clear();
-				$query->select('id');
-				$query->from('#__menu');
-				$query->where("path = '{$explode[0]}'");
-				$query->order('id ASC');
-				$query->limit(1);
-
-				$db->setQuery($query);
-				$parent = $db->loadResult();
-			}
-
-			// Resetting the table object
-			$table->reset();
-			// Setting the location of the new category
-			$table->setLocation($parent, 'last-child');
-			// Bind the data
-			$table->bind($menu);
-			// Store to database
-			$table->store();
+		try {
+			$this->_db->setQuery($query)->execute();
+		} catch (RuntimeException $e) {
+			throw new RuntimeException($e->getMessage());
 		}
 
-		$table->reset();
+		// Clear the default database
+		$query->clear();
+		$query->delete()->from('#__jupgradepro_default_menus')->where('id > 100');
 
-		if (!$table->rebuild()) {
-			echo JError::raiseError(500, $table->getError());
+		try {
+			$this->_db->setQuery($query)->execute();
+		} catch (RuntimeException $e) {
+			throw new RuntimeException($e->getMessage());
+		}
+
+		// Getting the menus
+		$query->clear();
+		// 3.0 Changes
+		if (version_compare(JUpgradeproHelper::getVersion('new'), '3.0', '>=')) {
+			$query->select("`menutype`, `title`, `alias`, `note`, `path`, `link`, `type`, `published`, `parent_id`, `component_id`, `checked_out`, `checked_out_time`, `browserNav`, `access`, `img`, `template_style_id`, `params`, `home`, `language`, `client_id`");
+		}else{
+			$query->select("`menutype`, `title`, `alias`, `note`, `path`, `link`, `type`, `published`, `parent_id`, `component_id`, `ordering`, `checked_out`, `checked_out_time`, `browserNav`, `access`, `img`, `template_style_id`, `params`, `home`, `language`, `client_id`");
+		}
+
+		$query->from("#__menu");
+		$query->where("id > 100");
+		$query->where("alias != 'home'");
+		$query->order('id ASC');
+		$this->_db->setQuery($query);
+
+		try {
+			$menus = $this->_db->loadObjectList();
+		} catch (RuntimeException $e) {
+			throw new RuntimeException($e->getMessage());
+		}
+
+		foreach ($menus as $menu)
+		{
+			// Convert the array into an object.
+			$menu = (object) $menu;
+
+			try {
+				$this->_db->insertObject('#__jupgradepro_default_menus', $menu);
+			} catch (RuntimeException $e) {
+				throw new RuntimeException($e->getMessage());
+			}
+		}
+
+		// Cleanup the entire menu
+		$query->clear();
+		$query->delete()->from('#__menu')->where('id > 1');
+
+		try {
+			$this->_db->setQuery($query)->execute();
+		} catch (RuntimeException $e) {
+			throw new RuntimeException($e->getMessage());
 		}
 	}
 
@@ -342,5 +326,94 @@ class JUpgradeproMenu extends JUpgradepro
 		}
 
 		return false;
+	}
+
+	/*
+	 * Fake method after hooks
+	 *
+	 * @return	void
+	 * @since	3.0.0
+	 * @throws	Exception
+	 */
+	public function afterHook()
+	{
+		$this->insertDefaultMenus();
+	}
+
+	/**
+	 * Insert the default menus deleted on cleanups to maintain the original id's
+	 *
+	 * @return	void
+	 * @since	0.5.2
+	 * @throws	Exception
+	 */
+	public static function insertDefaultMenus()
+	{
+		jimport('joomla.table.table');
+
+		// Getting the database instance
+		$db = JFactory::getDbo();
+		$query = $db->getQuery(true);
+		// Getting the table and query
+		$table = JTable::getInstance('Menu', 'JTable');
+
+		// Getting the data
+		$query->select('`menutype`, `title`, `alias`, `note`, `path`, `link`, `type`, `published`, `parent_id`, `component_id`, `ordering`, `checked_out`, `checked_out_time`, `browserNav`, `access`, `img`, `template_style_id`, `params`, `home`, `language`, `client_id`');
+		$query->from('#__jupgradepro_default_menus');
+		$query->order('id ASC');
+		$db->setQuery($query);
+		$menus = $db->loadAssocList();
+
+		foreach ($menus as $menu) {
+
+			// Unset id
+			$menu['id'] = 0;
+
+			// Getting the data
+			$query->clear();
+			$query->select('alias');
+			$query->from('#__menu');
+			$query->where("alias LIKE '{$menu['alias']}%'");
+			$query->order('id DESC');
+			$query->limit(1);
+			$db->setQuery($query);
+			$alias = $db->loadResult();
+
+			// Prevent MySQL duplicate error
+			// @@ Duplicate entry for key 'idx_client_id_parent_id_alias_language'
+			$menu['alias'] = (!empty($alias)) ? $alias."~" : $menu['alias'];
+
+			// Looking for parent
+			$parent = 1;
+			$explode = explode("/", $menu['path']);
+
+			if (count($explode) > 1) {
+
+				$query->clear();
+				$query->select('id');
+				$query->from('#__menu');
+				$query->where("path = '{$explode[0]}'");
+				$query->order('id ASC');
+				$query->limit(1);
+
+				$db->setQuery($query);
+				$parent = $db->loadResult();
+			}
+
+			// Resetting the table object
+			$table->reset();
+			// Setting the location of the new category
+			$table->setLocation($parent, 'last-child');
+			// Bind the data
+			$table->bind($menu);
+			// Store to database
+			$table->store();
+		}
+
+		$table->reset();
+
+		if (!$table->rebuild()) {
+			echo JError::raiseError(500, $table->getError());
+		}
 	}
 }

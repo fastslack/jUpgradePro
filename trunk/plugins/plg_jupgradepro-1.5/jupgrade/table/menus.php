a7d19823e5e163219516448a455f7c3646e17264
Matias Aguirre <maguirre@matware.com.ar>
2014-04-17 03:20:45 -0300

    Fix menus migration for 1.5 restful plugin

--- trunk/plugins/plg_jupgradepro-1.5/jupgrade/table/menus.php
+++ trunk/plugins/plg_jupgradepro-1.5/jupgrade/table/menus.php
@@ -91,86 +91,17 @@ class JUpgradeTableMenus extends JUpgradeTable
 		
 		$conditions['as'] = "m";
 		
-		$conditions['select'] = 'DISTINCT m.id, m.menutype, m.name, m.name AS title, m.alias, m.link, m.type, c.option, m.published, m.parent AS parent_id,'
-			.' m.sublevel AS level, m.ordering, m.checked_out, m.checked_out_time, m.browserNav, m.access, m.params, m.home';
+		$conditions['select'] = 'm.*, c.option, p.alias AS palias';
 		
 		$join = array();
 		$join[] = "LEFT JOIN #__components AS c ON c.id = m.componentid";
+		$join[] = "LEFT JOIN #__menu AS p ON p.id = m.parent";
 		
 		$conditions['where'] = array();
 		$conditions['join'] = $join;
-		$conditions['order'] = "m.id DESC";
+		$conditions['order'] = "m.id ASC";
 		
 		return $conditions;
-	}
-
-	/**
-	 * 
-	 *
-	 * @access	public
-	 * @param		Array	Result to migrate
-	 * @return	Array	Migrated result
-	 */
-	function migrate(&$rows)
-	{
-		foreach ($rows as $row)
-		{
-			// Fixing access
-			$row['access']++;
-			// Fixing level
-			$row['level']++;
-			// Fixing language
-			$row['language'] = '*';
-		  // Converting params to JSON
-		  $row['params'] = $this->convertParams($row['params']);
-			// Fixing parent_id
-			if (isset($row['parent_id'])) {
-				if ($row['parent_id'] == 0) {
-					$row['parent_id'] = 1;
-				}
-			}
-		
-		  // Fixing menus URLs
-		  if (strpos($row['link'], 'option=com_content') !== false) {
-
-		    if (strpos($row['link'], 'view=frontpage') !== false) {
-		      $row['link'] = 'index.php?option=com_content&view=featured';
-		    }
-		  }
-
-			if ( (strpos($row['link'], 'Itemid=') !== false) AND $row['type'] == 'menulink')
-			{
-
-				// Extract the Itemid from the URL
-				if (preg_match('|Itemid=([0-9]+)|', $row['link'], $tmp)) {
-					$item_id = $tmp[1];
-
-					$row['params'] = $row['params'] . "\naliasoptions=".$item_id;
-					$row['type'] = 'alias';
-					$row['link'] = 'index.php?Itemid=';
-				}
-			}
-
-			if (strpos($row['link'], 'option=com_user&') !== false)
-			{
-				$row['link'] = preg_replace('/com_user/', 'com_users', $row['link']);
-				$row['component_id'] = 25;
-				$row['option'] = 'com_users';
-
-				// Change the register view to registration
-				if (strpos($row['link'], 'view=register') !== false)
-				{
-					$row['link'] = 'index.php?option=com_users&view=registration';
-				}
-				else if (strpos($row['link'], 'view=user') !== false)
-				{
-					$row['link'] = 'index.php?option=com_users&view=profile';
-				}
-			}
-		  // End fixing menus URL's
-		}
-
-		return $rows;
 	}
 
 	/**

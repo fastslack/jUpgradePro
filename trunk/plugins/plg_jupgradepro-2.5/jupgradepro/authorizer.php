877deb75e020b06c07c31d435572f141ea071307
Matias Aguirre <maguirre@matware.com.ar>
2014-08-29 14:54:58 -0300

    Check if user key exists

--- trunk/plugins/plg_jupgradepro-2.5/jupgradepro/authorizer.php
+++ trunk/plugins/plg_jupgradepro-2.5/jupgradepro/authorizer.php
@@ -36,7 +36,15 @@ class JRESTAuthorizer
 		$client_key = trim($pluginParams->client_key);
 
 		// Uncrypt the request
-		$key = base64_decode($params['HTTP_KEY']);
+		$key = isset($params['HTTP_KEY']) ? base64_decode($params['HTTP_KEY']) : false;
+
+		if ($key === false) {
+			JResponse::setHeader('status', 402);
+			JResponse::setBody('Client key do not exists.');
+			JResponse::sendHeaders();
+			exit;
+		}
+
 		$parts	= explode( ':', $key );
 		$key	= trim($parts[0]);
 
